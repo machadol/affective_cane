dbcf45cf240723e075df92f49970632709e2cf0b
machadol <lucasmmsilva@gmail.com>
2017-10-15 18:12:36 -0200

    Biblioteca FSR

---
+++ Software/FSR/FSR.h
@@ -0,0 +1,18 @@
+#ifndef FSR_H
+#define FSR_H
+
+#include <FSR.h>
+
+class FSR_manager
+{
+	public:
+		FSR_manager(int analogport);
+		bool FSR_is_pressed();
+		int  FSR_current_value();
+	private:
+		int FSR_port;
+		int kernel_FSR(int analog);
+		bool state_of_button();
+		const static int button_level = 2;
+};
+#endif
